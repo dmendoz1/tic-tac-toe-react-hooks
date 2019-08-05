4b92a5489c4858798aa335cb28679ebfa79e65f0
Daniel Mendoza <dmendoza@fb.com>
2019-08-05 08:46:39 -0700

    Updates

---
+++ precache-manifest.682ba1f99a9a6b218ffd81a564d4715a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0a2fd4551cf3f3f5f53bff9ccb53d0",
+    "url": "/tic-tac-toe-react-hooks/index.html"
+  },
+  {
+    "revision": "690d4dec0ef1c6833453",
+    "url": "/tic-tac-toe-react-hooks/static/css/main.655f929b.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/tic-tac-toe-react-hooks/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "690d4dec0ef1c6833453",
+    "url": "/tic-tac-toe-react-hooks/static/js/main.310308a8.chunk.js"
+  },
+  {
+    "revision": "388d375f63d7d7ada925",
+    "url": "/tic-tac-toe-react-hooks/static/js/runtime~main.06165a0d.js"
+  }
+]);
